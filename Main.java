e62367da5a09691266d4cb8efba91ecca13d3637
Henrique_Pedrozo <168769340+Henrique-pedrozo@users.noreply.github.com>
2026-02-17 13:23:54 -0300

    Começando o projeto

---
+++ Main.java
@@ -0,0 +1,34 @@
+package SistemaDelivery;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Restaurante restaurante = new Restaurante();
+        Restaurante restaurante2 = new Restaurante();
+
+        System.out.println("Digite os nomes do restaurante");
+        System.out.println("1° Restaurante");
+        restaurante.nome = sc.nextLine();
+        System.out.println("2° Restaurante");
+        restaurante2.nome = sc.nextLine();
+
+        System.out.println("Digite os endereços dos restaurantes");
+        System.out.println("1° Restaurante");
+        restaurante.endereco = sc.nextLine();
+        System.out.println("2° Restaurante");
+        restaurante2.endereco = sc.nextLine();
+
+        System.out.println("Digite a categoria dos restaurante (Ex: Massas, Mexicano, Italiano)");
+        System.out.println("1° Restaurante");
+        restaurante.categoria = sc.nextLine();
+        System.out.println("2° Restaurante");
+
+        restaurante2.categoria = sc.nextLine();
+        System.out.println("Informações 1° restaurante");
+        restaurante.MostrarInformacoes();
+        System.out.println("Informações 2° restaurante");
+        restaurante2.MostrarInformacoes();
+    }
+}
---
+++ Restaurante.java
@@ -0,0 +1,37 @@
+package SistemaDelivery;
+
+public class Restaurante {
+     String nome;
+     String endereco;
+     String categoria;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    void MostrarInformacoes() {
+        System.out.println("Nome :" + nome);
+        System.out.println("Endereco :" + endereco);
+        System.out.println("Categoria :" + categoria);
+    }
+}
